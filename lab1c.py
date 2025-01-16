e85d8108d84e73ad75f37e80d93796eabb76a19f
mSeeps416 <mseeps416@outlook.com>
2025-01-16 10:20:32 +0000

    new changes

--- lab1c.py
+++ lab1c.py
@@ -12,7 +12,8 @@
 # Create a variable called 'radius' and take its value form user.
 # Convert the variable to integer using int()
 # use the contant pi form math module and compute the area of the circle using the variable 'radius'
-import math
-print (math.pi)
-radius = int (input("The radius: "))
-circumfrence = 
+
+import math                                             # Imported math module for pi
+radius = int (input("Enter the radius: "))                   # Have the user input a int for the radius
+circumfrence = 2 * math.pi * radius 
+print (circumfrence)                                        # Print the result
--- lab1d.py
+++ lab1d.py
@@ -2,8 +2,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Matthew Seepersaud
+# Date: 2025/ 01/ 15
 # Purpose: Use string methods and f-string formating.
 # Usage: ./lab1d.py
 
@@ -13,11 +13,16 @@
 # Create another variable called “age”, the value of “age” should be your age
 # The script, when executed, should print out "How are you yourname? Happy xxth birthday!" To print this output use .format() method. 
 
+name = "Matthew"
+uppercase_text = name.upper()
+print (uppercase_text)
 #TO-DO 2:
 # Create a variable called "words".
 # The value of words should be "The quick brown fox jumps over the lazy dog".
 # Use indexing to return the first and 17th charecters of "words" to the user.
-
+words = "The quick brown fox jumps oer the lazy dog"
+characters = words [0:17:16]
+print (characters)
 #TO-DO 3:
 # Use negative indexing to return the words "jumps" and "quick" from "words" to the user.
 
